79a0da6ec7dd9e95ec4149fb9d30de7f44bb1637
Ely Karr <elykarr@gmail.com>
2022-04-15 13:01:02 -0700

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorize
+
+
+  private
+
+      def authorize
+        return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+      end
+
+      def current_user
+        @current_user = User.find_by(id: session[:user_id])
+      end
 
 end
